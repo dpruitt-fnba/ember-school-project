ecb530a110634cf3f10a238033e8fc9c3d72a602
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 11:24:16 -0400

    Get chef data from firebase

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,27 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [
-      {
-        name: 'Gordon Ramsay',
-        isCookingToday: true
-      },
-      {
-        name: 'Anthony Bourdain',
-        isCookingToday: false
-      },
-      {
-        name: 'Rachael Ray',
-        isCookingToday: true
-      },
-      {
-        name: 'Jamie Oliver',
-        isCookingToday: false
-      },
-      {
-        name: 'Guy Fieri',
-        isCookingToday: true
-      }
-    ];
+    return this.store.findAll('chef');
   }
 });
