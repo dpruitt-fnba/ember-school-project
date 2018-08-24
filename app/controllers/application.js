1b15e9f24c13e8330d68d300b435c11ccc4fd775
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 12:03:54 -0400

    Add create chef functionality

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { set } from '@ember/object';
 
 export default Controller.extend({
+  newChef: null,
   actions: {
     enter(chef) {
       set(chef, 'isCookingToday', true);
@@ -10,6 +11,13 @@ export default Controller.extend({
     exit(chef) {
       set(chef, 'isCookingToday', false);
       chef.save();
+    },
+    addNewChef() {
+      this.store.createRecord('chef', {
+        name: this.get('newChef'),
+        isCookingToday: false
+      }).save();
+      this.set('newChef', null);
     }
   }
 });
