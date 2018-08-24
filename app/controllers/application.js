598c95be91085544f110adb770231c75eef85b3c
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 10:55:33 -0400

    Move chef data to route model

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,28 +2,6 @@ import Controller from '@ember/controller';
 import { set } from '@ember/object';
 
 export default Controller.extend({
-  chefs: [
-    {
-      name: 'Gordon Ramsay',
-      isCookingToday: true
-    },
-    {
-      name: 'Anthony Bourdain',
-      isCookingToday: false
-    },
-    {
-      name: 'Rachael Ray',
-      isCookingToday: true
-    },
-    {
-      name: 'Jamie Oliver',
-      isCookingToday: false
-    },
-    {
-      name: 'Guy Fieri',
-      isCookingToday: true
-    }
-  ],
   actions: {
     enter(chef) {
       set(chef, 'isCookingToday', true);
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        name: 'Gordon Ramsay',
+        isCookingToday: true
+      },
+      {
+        name: 'Anthony Bourdain',
+        isCookingToday: false
+      },
+      {
+        name: 'Rachael Ray',
+        isCookingToday: true
+      },
+      {
+        name: 'Jamie Oliver',
+        isCookingToday: false
+      },
+      {
+        name: 'Guy Fieri',
+        isCookingToday: true
+      }
+    ];
+  }
 });
