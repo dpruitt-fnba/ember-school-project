e7be3b942c66adcc6be2ed5ea646520214b5f42e
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 14:41:47 -0400

    Move chef logic/template to new chefs controller/template

---
+++ app/controllers/chefs.js
@@ -0,0 +1,30 @@
+import Controller from '@ember/controller';
+import { set } from '@ember/object';
+import { filterBy } from '@ember/object/computed';
+import { alias } from '@ember/object/computed';
+
+export default Controller.extend({newChef: null,
+  chefs: alias('model'),
+  activeChefs: filterBy('model', 'isCookingToday', true),
+  actions: {
+    enter(chef) {
+      set(chef, 'isCookingToday', true);
+      chef.save();
+    },
+    exit(chef) {
+      set(chef, 'isCookingToday', false);
+      chef.save();
+    },
+    addNewChef() {
+      this.store.createRecord('chef', {
+        name: this.get('newChef'),
+        isCookingToday: false
+      }).save();
+      this.set('newChef', null);
+    },
+    fireChef(chef) {
+      chef.destroyRecord();
+    }
+  }
+
+});
