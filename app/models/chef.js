e3c6d4c8b882a6f66b9a1a8f81b8404492c46955
Dylan Pruitt <dpruitt@fnba.com>
2018-09-11 14:22:34 -0400

    Set up relationships on chef and restaurant models

--- app/models/chef.js
+++ app/models/chef.js
@@ -1,8 +1,10 @@
 import DS from 'ember-data';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   isCookingToday: DS.attr('boolean'),
   description: DS.attr('string'),
-  imageUrl: DS.attr('string')
+  imageUrl: DS.attr('string'),
+  restaurant: DS.belongsTo()
 });
--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  chefs: DS.hasMany()
 });
