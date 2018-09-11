3b625d313c4ab4517a1f7b3d5b401e21b049dcca
Dylan Pruitt <dpruitt@fnba.com>
2018-09-11 15:12:54 -0400

    Update restaurant edit route to pull in chefs

--- app/controllers/restaurants/restaurant/edit.js
+++ app/controllers/restaurants/restaurant/edit.js
@@ -2,7 +2,8 @@ import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
-  restaurant: alias('model'),
+  restaurant: alias('model.restaurant'),
+  chefs: alias('model.chefs'),
   actions: {
     save() {
       this.get('restaurant').save();
--- app/routes/restaurants/restaurant/edit.js
+++ app/routes/restaurants/restaurant/edit.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   model(params) {
-    return this.store.findRecord('restaurant', params.id);
+    return RSVP.hash({
+      restaurant: this.store.findRecord('restaurant', params.id),
+      chefs: this.store.findAll('chef')
+    });
   }
 });
