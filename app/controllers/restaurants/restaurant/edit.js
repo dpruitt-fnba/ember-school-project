638511c65ba17cb9f5d2746d5a557c685f43e359
Dylan Pruitt <dpruitt@fnba.com>
2018-09-11 15:15:41 -0400

    Update edit restaurant template to display working chefs and other chefs

--- app/controllers/restaurants/restaurant/edit.js
+++ app/controllers/restaurants/restaurant/edit.js
@@ -1,9 +1,10 @@
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
+import { alias, setDiff } from '@ember/object/computed';
 
 export default Controller.extend({
   restaurant: alias('model.restaurant'),
   chefs: alias('model.chefs'),
+  otherChefs: setDiff('chefs', 'restaurant.chefs'),
   actions: {
     save() {
       this.get('restaurant').save();
